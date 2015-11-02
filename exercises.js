7d1cd6f0f39dfd6302c1ede96b7ba2e6c63f0cef
JohnAndScott <johnandscott@appacademy.io>
2015-11-02 11:43:58 -0500

    complete curry function prototype.

--- exercises.js
+++ exercises.js
@@ -42,7 +42,6 @@ var curriedSum = function (numArgs) {
       var finalSum = 0;
       numbers.forEach(function(el) {
         finalSum += el;
-
       });
       return finalSum;
     }
@@ -52,8 +51,17 @@ var curriedSum = function (numArgs) {
   };
 return _curriedSum;
 }
-// Function.prototype.curry = function(numArgs){
-//
-//
-//
-// }
+Function.prototype.curry = function(numArgs){
+  var args = [];
+  var fn = this;
+  var _curry = function(arg){
+    args.push(arg);
+    if (args.length === numArgs){
+      return fn.apply(fn, args);
+    }
+    else{
+      return _curry;
+    }
+  };
+    return _curry;
+}
