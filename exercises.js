82af76905834252f77cc04e1bf9d8f6139a43194
JohnAndScott <johnandscott@appacademy.io>
2015-11-02 10:59:41 -0500

    Implement myBind with args.

--- exercises.js
+++ exercises.js
@@ -7,3 +7,28 @@ var sum = function () {
 
   return sum;
 };
+
+Function.prototype.myBind = function (context) {
+  var fn = this;
+  args = Array.prototype.slice.call(arguments, 1);
+  return function () {
+    fn.apply(context, args);
+  };
+};
+
+// function Cat(name) {
+//   this.name = name;
+// };
+//
+// Cat.prototype.says = function (sound) {
+//   console.log(this.name + " says " + sound + "!");
+// }
+//
+// markov = new Cat("Markov");
+// breakfast = new Cat("Breakfast");
+//
+// markov.says("meow");
+// // Markov says meow!
+//
+// markov.says.myBind(breakfast, "WOOF")();
+// // Breakfast says meow!
